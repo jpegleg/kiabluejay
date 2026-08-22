4620fc3cdd1d99caa2bee51180305cccc202d2c2
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2026-08-22 00:27:04 -0400

    include kv-log-macro functionality within json_events.rs

--- src/json_events.rs
+++ src/json_events.rs
@@ -1,10 +1,191 @@
+pub use log::Level;
+use log::{LevelFilter, Record, kv, logger};
+use std::fmt;
 pub extern crate env_logger;
 use env_logger::Builder;
-use log::{LevelFilter, kv};
 use std::{
     io::{self, Write},
     panic, thread,
 };
+
+pub const STATIC_MAX_LEVEL: LevelFilter = log::STATIC_MAX_LEVEL;
+
+#[inline]
+pub fn max_level() -> LevelFilter {
+    log::max_level()
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! log {
+    (target: $target:expr, $lvl:expr, $e:expr) => {
+        $crate::log_impl!(target: $target, $lvl, ($e));
+    };
+    (target: $target:expr, $lvl:expr, $e:expr, $($rest:tt)*) => {
+        $crate::log_impl!(target: $target, $lvl, ($e) $($rest)*);
+    };
+    ($lvl:expr, $($arg:tt)+) => ($crate::log!(target: __log_module_path!(), $lvl, $($arg)+))
+}
+
+#[macro_export(local_inner_macros)]
+#[doc(hidden)]
+macro_rules! log_impl {
+    (target: $target:expr, $lvl:expr, ($($arg:expr),*)) => {{
+        let lvl = $lvl;
+        if lvl <= $crate::STATIC_MAX_LEVEL && lvl <= $crate::max_level() {
+            $crate::__private_api_log(
+                __log_format_args!($($arg),*),
+                lvl,
+                &($target, __log_module_path!(), __log_file!(), __log_line!()),
+                None,
+            );
+        }
+    }};
+
+    (target: $target:expr, $lvl:expr, ($($arg:expr),*) { $($key:ident : $value:expr),* }) => {{
+        if $lvl <= STATIC_MAX_LEVEL && $lvl <= max_level() {
+            __private_api_log(
+                __log_format_args!($($arg),*),
+                $lvl,
+                &(__log_module_path!(), __log_module_path!(), __log_file!(), __log_line!()),
+                Some(&[$((__log_stringify!($key), &$value)),*])
+            );
+        }
+    }};
+
+    (target: $target:expr, $lvl:expr, ($($e:expr),*) { $($key:ident : $value:expr,)* }) => {
+        $crate::log_impl!(target: $target, $lvl, ($($e),*) { $($key : $value),* });
+    };
+
+    (target: $target:expr, $lvl:expr, ($($e:expr),*) $arg:expr) => {
+        $crate::log_impl!(target: $target, $lvl, ($($e,)* $arg));
+    };
+
+    (target: $target:expr, $lvl:expr, ($($e:expr),*) $arg:expr, $($rest:tt)*) => {
+        $crate::log_impl!(target: $target, $lvl, ($($e,)* $arg) $($rest)*);
+    };
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! trace {
+    (target: $target:expr, $($arg:tt)+) => (
+        log!(target: $target, Level::Trace, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        log!(Level::Trace, $($arg)+);
+    )
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! debug {
+    (target: $target:expr, $($arg:tt)+) => (
+        log!(target: $target, $crate::Level::Debug, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        log!($crate::Level::Debug, $($arg)+);
+    )
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! info {
+    (target: $target:expr, $($arg:tt)+) => (
+        log!(target: $target, $crate::Level::Info, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        log!($crate::Level::Info, $($arg)+);
+    )
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! warn {
+    (target: $target:expr, $($arg:tt)+) => (
+        log!(target: $target, $crate::Level::Warn, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        log!($crate::Level::Warn, $($arg)+);
+    )
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! error {
+    (target: $target:expr, $($arg:tt)+) => (
+        log!(target: $target, $crate::Level::Error, $($arg)+);
+    );
+    ($($arg:tt)+) => (
+        log!(Level::Error, $($arg)+);
+    )
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! log_enabled {
+    (target: $target:expr, $lvl:expr) => {{
+        let lvl = $lvl;
+        lvl <= $crate::STATIC_MAX_LEVEL
+            && lvl <= $crate::max_level()
+            && $crate::__private_api_enabled(lvl, $target)
+    }};
+    ($lvl:expr) => {
+        log_enabled!(target: __log_module_path!(), $lvl)
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_format_args {
+    ($($args:tt)*) => {
+        format_args!($($args)*)
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_module_path {
+    () => {
+        module_path!()
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_file {
+    () => {
+        file!()
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_line {
+    () => {
+        line!()
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __log_stringify {
+    ($($args:tt)*) => {
+        stringify!($($args)*)
+    };
+}
+
+pub fn __private_api_log(
+    args: fmt::Arguments<'_>,
+    level: Level,
+    &(target, module_path, file, line): &(&str, &'static str, &'static str, u32),
+    kvs: Option<&[(&str, &dyn log::kv::ToValue)]>,
+) {
+    logger().log(
+        &Record::builder()
+            .args(args)
+            .level(level)
+            .target(target)
+            .module_path_static(Some(module_path))
+            .file_static(Some(file))
+            .line(Some(line))
+            .key_values(&kvs)
+            .build(),
+    );
+}
 
 pub fn panic_catch() {
     panic::set_hook(Box::new(|info| {
@@ -19,7 +200,7 @@ pub fn panic_catch() {
         };
         match info.location() {
             Some(location) => {
-                kv_log_macro::error!(
+                error!(
                     "panicked at '{}'", errout,
                     {
                         thread: thread,
@@ -28,7 +209,7 @@ pub fn panic_catch() {
                 );
             }
             None => {
-                kv_log_macro::error!("panicked at '{}'", errout, { thread: thread });
+                error!("panicked at '{}'", errout, { thread: thread });
             }
         }
     }));
